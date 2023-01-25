8ac1792ebb156df13424b5c705ffae65008b9b0c
Sudipto Shahin <sudipto.shine@gmail.com>
2023-01-26 00:22:53 +0600

    project-setup

---
+++ src/components/Todos.vue
@@ -0,0 +1,17 @@
+/* eslint-disable */
+<template>
+    <div>
+        <h3>Todos</h3>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Todos',
+}
+</script>
+
+<style>
+
+</style>
